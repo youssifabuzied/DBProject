8376b738fa6975b25c88eb6e47a18e1c33243724
youssifabuzied <90260972+youssifabuzied@users.noreply.github.com>
2023-04-03 22:02:44 +0200

    .

---
+++ database_project_user.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: database_project
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('Salma_Omar','a11@gmail.com','1997-08-14','f'),('Salma_Omar','a14@gmail.com','1969-10-13','f'),('Fatma_Mohamed','a18@gmail.com','1998-04-22','f'),('Omar_youssif','a41@gmail.com','1979-05-12','m'),('youssif_Ahmed','a54@gmail.com','1994-06-16','m'),('Mariam_Omar','a65@gmail.com','1986-03-04','f'),('Salma_youssif','a7@gmail.com','1985-09-06','f'),('Salma_Omar','a71@gmail.com','1971-11-11','f'),('Mariam_Ahmed','a79@gmail.com','1976-01-26','f'),('Salma_Ahmed','a99@gmail.com','1999-06-23','f'),('Mohamed_Mohamed','b28@gmail.com','1985-07-21','m'),('Mariam_Omar','b37@gmail.com','1984-04-13','f'),('Salah_Mohamed','b93@gmail.com','1967-06-18','m'),('Fatma_Omar','c24@gmail.com','1970-05-01','f'),('Omar_Ahmed','c48@gmail.com','1964-01-14','m'),('Omar_Mohamed','c58@gmail.com','1987-02-10','m'),('Omar_Mohamed','d1@gmail.com','1963-03-02','m'),('Mariam_Ahmed','d29@gmail.com','1967-12-02','f'),('Omar_youssif','d3@gmail.com','1968-05-22','m'),('Salma_Omar','d40@gmail.com','1987-05-20','f'),('Mohamed_Salah','d78@gmail.com','1978-06-28','m'),('Ahmed_youssif','e32@gmail.com','2004-09-06','m'),('Salma_youssif','e72@gmail.com','1987-09-26','f'),('Mariam_Salah','f26@gmail.com','1999-11-22','f'),('Fatma_Mohamed','f59@gmail.com','1971-04-13','f'),('Fatma_Ahmed','f76@gmail.com','1977-05-03','f'),('Omar_youssif','f96@gmail.com','1989-03-11','m'),('Ahmed_Salah','g0@gmail.com','1976-06-25','m'),('Ahmed_Salah','g23@gmail.com','1975-06-02','m'),('youssif_Salah','g50@gmail.com','1964-03-18','m'),('Noura_Omar','g60@gmail.com','1985-04-04','f'),('Salah_Mohamed','g82@gmail.com','1975-06-01','m'),('Mohamed_Mohamed','h43@gmail.com','1984-08-20','m'),('Salma_youssif','h73@gmail.com','1981-03-06','f'),('youssif_Omar','h75@gmail.com','1965-09-15','m'),('Fatma_Mohamed','h80@gmail.com','1991-06-20','f'),('Noura_Ahmed','h89@gmail.com','1975-04-13','f'),('Salma_youssif','i25@gmail.com','1982-11-04','f'),('Mohamed_Mohamed','i87@gmail.com','1960-09-01','m'),('Omar_Mohamed','i9@gmail.com','2005-11-24','m'),('Salma_youssif','j13@gmail.com','1960-06-15','f'),('youssif_Mohamed','j19@gmail.com','1998-06-07','m'),('youssif_Omar','j21@gmail.com','1964-09-18','m'),('Fatma_Omar','j64@gmail.com','1980-10-28','f'),('Rana_Mohamed','j94@gmail.com','1963-02-26','f'),('Salma_youssif','k2@gmail.com','1982-02-23','f'),('Salah_Omar','k30@gmail.com','1993-01-21','m'),('Mariam_youssif','k35@gmail.com','1988-12-07','f'),('Salah_Salah','k63@gmail.com','2005-07-05','m'),('Rana_Salah','k69@gmail.com','1965-09-14','f'),('Noura_Omar','l45@gmail.com','1986-02-18','f'),('Salah_Omar','l51@gmail.com','1965-07-27','m'),('Salma_Ahmed','l61@gmail.com','1975-07-03','f'),('Salma_Ahmed','l83@gmail.com','2003-07-07','f'),('Fatma_Salah','l97@gmail.com','1995-12-17','f'),('Mohamed_Omar','n85@gmail.com','1969-07-01','m'),('Salah_Mohamed','n91@gmail.com','1991-12-26','m'),('Noura_youssif','n98@gmail.com','1961-12-04','f'),('Salma_Ahmed','o27@gmail.com','1976-03-16','f'),('Omar_Salah','o4@gmail.com','2004-05-28','m'),('Mariam_Omar','o47@gmail.com','1987-04-26','f'),('Mariam_Salah','o68@gmail.com','1986-01-07','f'),('Salma_Ahmed','o88@gmail.com','1998-07-26','f'),('Mohamed_youssif','p20@gmail.com','1968-03-10','m'),('youssif_Omar','p62@gmail.com','1966-07-04','m'),('Salma_Ahmed','p66@gmail.com','1978-08-10','f'),('Ahmed_Ahmed','p74@gmail.com','1963-12-01','m'),('Mohamed_Omar','q34@gmail.com','1996-08-01','m'),('Salah_Mohamed','q77@gmail.com','1972-07-09','m'),('Salma_Ahmed','q90@gmail.com','1962-09-25','f'),('Omar_Omar','q95@gmail.com','2004-04-10','m'),('Mohamed_Omar','r17@gmail.com','2004-12-18','m'),('Rana_Salah','r42@gmail.com','2002-11-08','f'),('Ahmed_Ahmed','s22@gmail.com','2000-01-15','m'),('Salma_Salah','s49@gmail.com','1978-08-05','f'),('Mariam_Mohamed','s53@gmail.com','1988-11-24','f'),('Omar_youssif','s84@gmail.com','1983-11-10','m'),('Salah_Omar','s86@gmail.com','1971-11-02','m'),('Fatma_Ahmed','t39@gmail.com','1979-01-26','f'),('Salma_Ahmed','t44@gmail.com','1973-04-20','f'),('Noura_Salah','u10@gmail.com','1994-09-15','f'),('Salma_Omar','u15@gmail.com','1969-11-25','f'),('Mariam_Ahmed','u36@gmail.com','2002-05-18','f'),('Rana_Mohamed','u52@gmail.com','2001-07-03','f'),('Salah_Mohamed','u57@gmail.com','1979-05-17','m'),('youssif_Salah','v38@gmail.com','1980-01-25','m'),('Fatma_Salah','v8@gmail.com','1976-01-22','f'),('Ahmed_Mohamed','w33@gmail.com','1972-09-16','m'),('youssif_Omar','w56@gmail.com','2000-09-03','m'),('Fatma_youssif','w70@gmail.com','1975-05-25','f'),('Ahmed_Ahmed','w92@gmail.com','1970-06-05','m'),('Noura_youssif','x6@gmail.com','1984-05-05','f'),('Noura_Mohamed','x67@gmail.com','1982-12-17','f'),('Fatma_youssif','y16@gmail.com','1990-12-05','f'),('Ahmed_youssif','y31@gmail.com','1997-08-07','m'),('Salma_Ahmed','y46@gmail.com','1975-02-09','f'),('Mohamed_Ahmed','y81@gmail.com','1999-07-14','m'),('youssif_youssif','z12@gmail.com','2005-05-07','m'),('Rana_Salah','z5@gmail.com','1990-07-18','f'),('Mariam_Mohamed','z55@gmail.com','2002-07-13','f');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-27 20:24:32
